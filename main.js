1584bd55c4c08ca8ad4e87ffa02224cc570be4d5
unknown-user124 <73541799+unknown-user124@users.noreply.github.com>
2021-03-15 14:55:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var prediction_1="";
+var prediction_2="";
+Webcam.set ({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+   
+function captureimage() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="captured_image"  src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version: ' , ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ABeO04Q7f/model.json',modelLoaded);
+
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak() {
+    var synth = window.speechSytnthesis;
+    speak_data_1="The first prediction is"+ prediction_1;
+    speak_data_1="The second prediction is"+ prediction_2;
+    var utterThis =new SpeechSynthesisisUtterance(speak_data_1+ speak_data_2);
+    synth.speak(utterThis);
+}
